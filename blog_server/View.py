f33e9745674df7596d631fe34d1fc305391b0e1c
Asaf Baibekov <asaf.baibekov@stu.shenkar.ac.il>
2023-05-20 22:12:39 +0300

    extract html text to html files

--- blog_server/View.py
+++ blog_server/View.py
@@ -1,77 +1,30 @@
 class View:
     def __init__(self, model):
         self.model = model
-
     
     def render_home(self, comments):
+        with open('public/home_template.html', 'r') as file:
+            home_html = file.read()
+        
+        # Modify the HTML content as needed
         comment_template = "<li>{}</li>"
         comment_list_html = ''.join(comment_template.format(comment) for comment in comments)
-        home_html = f'''
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>My Website</title>
-        </head>
-        <body>
-            <h1>Welcome to my website!</h1>
-            <img src="/Flapjack.jpg" alt="Flapjack" style="width:400px;">
-            <br>
-            <h2>Leave a comment:</h2>
-            <form action="/comment" method="POST">
-                <input type="text" name="comment">
-                <input type="submit" value="Submit">
-            </form>
-            <h2>Comments:</h2>
-            {comment_list_html}
-            <a href="/logout">Logout</a>
-        </body>
-        </html>
-        '''
+        home_html = home_html.replace('{comment_list_html}', comment_list_html)
+        
         return home_html
     
     def render_index(self):
-        index_html = """
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <meta charset="utf-8">
-                <title>Login</title>
-            </head>
-            <body>
-                <h1>Login</h1>
-                <form action="login" method="POST">
-                <div>
-                    <label for="username">Username:</label>
-                    <input type="text" id="username" name="username">
-                </div>
-                <div>
-                    <label for="password">Password:</label>
-                    <input type="password" id="password" name="password">
-                </div>
-                <div>
-                    <button type="submit">Login</button>
-                    <button type="submit" formaction="/register">Sign Up</button>
-                </div>
-                </form>
-                <script>
-                </script>
-            </body>
-            </html>
-        """
+        with open('public/index_template.html', 'r') as file:
+            index_html = file.read()
+        
         return index_html
     
-    def error_html(self, code, body):
-        return f"""
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>Error</title>
-        </head>
-        <body>
-            <h1>Error {code}</h1>
-            <p>{body}</p>
-            <a href="/home">Go home</a>
-        </body>
-        </html>
-        """
-
+    def render_error(self, code, body):
+        with open('public/error_template.html', 'r') as file:
+            error_html = file.read()
+        
+        # Modify the HTML content as needed
+        error_html = error_html.replace('{code}', str(code))
+        error_html = error_html.replace('{body}', body)
+        
+        return error_html
