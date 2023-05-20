a0dae543eafe983f097408df3e6cb969f579385b
Asaf Baibekov <asaf.baibekov@stu.shenkar.ac.il>
2023-05-20 21:26:17 +0300

    extract texts html into html files

--- eval_server/View.py
+++ eval_server/View.py
@@ -3,103 +3,22 @@ class View:
         self.model = model
     
     def render_index(self, cookies):
+        with open('public/index_template.html', 'r') as file:
+            index_html = file.read()
+        
+        # Modify the HTML content as needed
         cookie_template = '<div class="card"><p>cookie: {}<br>origin: {}</p></div>'
         cookies_list_html = ''.join(cookie_template.format(*cookie.rsplit('->', 1)) for cookie in cookies)
-        index_html = f"""
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <title>Hello World</title>
-                <style>
-                    body {{
-                        background-image: url('/background.jpg');
-                        background-size: cover;
-                        background-repeat: no-repeat;
-                        min-height: 100vh;
-                         margin: 0;
-                    }}
-                    h1 {{
-                        text-align: center;
-                        color: white;
-                        font-size: 4em;
-                        padding-top: 100px;
-                        margin: 0;
-                    }}
-                    h3 {{
-                        text-align: center;
-                        color: white;
-                        font-size: 2.5em;
-                    }}
-                    .container {{
-                        display: flex;
-                        justify-content: center;
-                        align-items: center;
-                        max-height: 50vh;
-                        flex-wrap: wrap;
-                        overflow: scroll;
-                    }}
-                    .card {{
-                        background-color: rgba(0, 0, 0, 0.8);
-                        border-radius: 10px;
-                        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.4);
-                        cursor: pointer;
-                        margin: 20px;
-                        padding: 50px;
-                        transition: transform 0.2s ease-in-out;
-                    }}
-                    .card:hover {{
-                        transform: translateY(-10px);
-                    }}
-                    .card p {{
-                        color: white;
-                        font-size: 1.5em;
-                        margin: 0;
-                    }}
-
-                </style>
-            </head>
-            <body>
-                <h1>Hello world</h1>
-                <h3>All the cookies that have been captured so far</h3>
-                <div class="container">
-                    {cookies_list_html}
-                </div>
-            </body>
-            </html>
-        """
+        index_html = index_html.replace('{cookies_list}', cookies_list_html)
+        
         return index_html
     
     def render_error(self, code, body):
-        error_html = f"""
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <title>Error {code}</title>
-                <style>
-                    body {{
-                        background-image: url('/background.jpg');
-                        background-size: cover;
-                        background-repeat: no-repeat;
-                        min-height: 100vh;
-                         margin: 0;
-                    }}
-                    h1 {{
-                        text-align: center;
-                        color: white;
-                        font-size: 4em;
-                        padding-top: 100px;
-                        margin: 0;
-                    }}
-                    p {{
-                        text-align: center;
-                    }}
-                </style>
-            </head>
-            <body>
-                <h1>Error {code}</h1>
-                <p>{body}</p>
-            </body>
-            </html>
-        """
-        return error_html
+        with open('public/error_template.html', 'r') as file:
+            error_html = file.read()
         
+        # Modify the HTML content as needed
+        error_html = error_html.replace('{code}', str(code))
+        error_html = error_html.replace('{body}', body)
+        
+        return error_html
